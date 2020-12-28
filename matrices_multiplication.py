f07edc75dfdd17c17cf3dc392292c03196344bad
Serhii Lysovenko <Lysovenko@users.noreply.github.com>
2020-12-28 18:34:40 +0200

    perf_counter

--- matrices_multiplication.py
+++ matrices_multiplication.py
@@ -1,7 +1,7 @@
 import pyopencl as cl
 import numpy as np
 from misc import create_some_context, load_cl_text
-from time import time
+from time import perf_counter
 
 
 (n, m, p) = (3, 4, 5)
@@ -9,7 +9,7 @@ from time import time
 a = np.random.randint(2, size=(n*m)).astype(np.float32)
 b = np.random.randint(2, size=(m*p)).astype(np.float32)
 c = np.zeros((n*p), dtype=np.float32)
-
+TIMES = {}
 ctx = create_some_context()
 queue = cl.CommandQueue(ctx)
 
@@ -17,14 +17,18 @@ mf = cl.mem_flags
 a_buf = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=a)
 b_buf = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=b)
 c_buf = cl.Buffer(ctx, mf.WRITE_ONLY, c.nbytes)
+pt = perf_counter()
 prg = cl.Program(ctx, load_cl_text("multiply_matr.cl")).build()
-st = time()
+TIMES["Compilation"] = perf_counter() - pt
+pt = perf_counter()
 prg.multiply(queue, c.shape, None,
              np.uint16(n), np.uint16(m), np.uint16(p),
              a_buf, b_buf, c_buf)
+TIMES["Execution"] = perf_counter() - pt
 a_mul_b = np.empty_like(c)
+pt = perf_counter()
 cl.enqueue_copy(queue, a_mul_b, c_buf)
-elapsed = time() - st
+TIMES["Copying"] = perf_counter() - pt
 
 print("matrix A:")
 print(a.reshape(n, m))
@@ -32,4 +36,4 @@ print("matrix B:")
 print(b.reshape(m, p))
 print("multiplied A*B:")
 print(a_mul_b.reshape(n, p))
-print(elapsed)
+print("\n".join("%s:\t%g" % i for i in TIMES.items()))
